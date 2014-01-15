f2e2a64eea3847ae13d5dc80fb8a1229b24bf5f5
Emile Castelnuovo <emile.castelnuovo@gmail.com>
2014-01-15 12:11:25 +0100

    [UPDATE] Small fixes

--- Acopter32-STM32F4/libraries/AP_HAL_VRBRAIN/HAL_VRBRAIN_Class.cpp
+++ Acopter32-STM32F4/libraries/AP_HAL_VRBRAIN/HAL_VRBRAIN_Class.cpp
@@ -23,8 +23,8 @@ using namespace VRBRAIN;
 
 // XXX make sure these are assigned correctly
 static VRBRAINUARTDriver uartADriver(_USART1,1);
-static VRBRAINUARTDriver uartBDriver(_USART2,0);
-static VRBRAINUARTDriver uartCDriver(_USART3,0);
+static VRBRAINUARTDriver uartBDriver(_USART3,0);
+static VRBRAINUARTDriver uartCDriver(_USART2,0);
 static VRBRAINSemaphore  i2cSemaphore;
 static VRBRAINSemaphore  i2c2Semaphore;
 static VRBRAINI2CDriver  i2cDriver(_I2C2,&i2cSemaphore);
@@ -51,7 +51,7 @@ HAL_VRBRAIN::HAL_VRBRAIN() :
       &spiDeviceManager,
       &analogIn,
       &storageDriver,
-      &uartCDriver,
+      &uartADriver,
       &gpioDriver,
       &rcinDriver,
       &rcoutDriver,
