a7641d772ee85c15847e9889a7f23a8256ea773e
Emile Castelnuovo <emile.castelnuovo@gmail.com>
2014-03-11 22:06:34 +0100

    [FIX] fixed some dataflash mavlink log download

--- Acopter32-STM32F4/libraries/AP_Math/location.cpp
+++ Acopter32-STM32F4/libraries/AP_Math/location.cpp
@@ -180,6 +180,34 @@ int32_t wrap_180_cd(int32_t error)
 }
 
 /*
+  wrap an angle in centi-degrees to 0..35999
+ */
+float wrap_360_cd_float(float angle)
+{
+    if (angle >= 72000.0f || angle < -36000.0f) {
+        // for larger number use fmodulus
+        angle = fmod(angle, 36000.0f);
+    }
+    if (angle >= 36000.0f) angle -= 36000.0f;
+    if (angle < 0.0f) angle += 36000.0f;
+    return angle;
+}
+
+/*
+  wrap an angle in centi-degrees to -18000..18000
+ */
+float wrap_180_cd_float(float angle)
+{
+    if (angle > 54000.0f || angle < -54000.0f) {
+        // for large numbers use modulus
+        angle = fmod(angle,36000.0f);
+    }
+    if (angle > 18000.0f) { angle -= 36000.0f; }
+    if (angle < -18000.0f) { angle += 36000.0f; }
+    return angle;
+}
+
+/*
   wrap an angle defined in radians to -PI ~ PI (equivalent to +- 180 degrees)
  */
 float wrap_PI(float angle_in_radians)
