82c8aa5fece728e385f4c1759fa77cd620a5f1c6
Emile Castelnuovo <emile.castelnuovo@gmail.com>
2014-05-10 12:17:27 +0200

    Foolw Me: enable LOGGING of follow me messages by default.

--- Acopter32-STM32F4/ArduCopter/config.h
+++ Acopter32-STM32F4/ArduCopter/config.h
@@ -971,7 +971,8 @@
     MASK_LOG_RCIN | \
     MASK_LOG_CMD | \
     MASK_LOG_CURRENT | \
-    MASK_LOG_ROI
+    MASK_LOG_ROI |\
+    MASK_LOG_FOLLOWME
 #else
  // PX4, Pixhawk, FlyMaple default logging
  # define DEFAULT_LOG_BITMASK \
